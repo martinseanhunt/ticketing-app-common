1bb070e0a3f7869aba53d7e0b8e524eb50ef1715
Martin Hunt <martinseanhunt@gmail.com>
2020-11-17 18:58:39 +0000

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// errors
+// Errors
 export * from './errors/AuthorizationError'
 export * from './errors/BadRequestError'
 export * from './errors/CustomError'
@@ -6,7 +6,7 @@ export * from './errors/DatabaseConnectionError'
 export * from './errors/NotFoundError'
 export * from './errors/RequestValidationError'
 
-// middlewares
+// Middlewares
 export * from './middlewares/currentUser'
 export * from './middlewares/errorHandler'
 export * from './middlewares/handleValidationErrors'
